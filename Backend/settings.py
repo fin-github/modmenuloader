17fa26e8507eb1e1fbda15e9260210585aae9548
Fin-GITHUB <70870542+fin-github@users.noreply.github.com>
2023-12-23 16:37:27 -0500

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -4,7 +4,8 @@ from Backend.mods import *
 class Settingsclass():
     def get_settings(self):
         return (
-            # USE HERE!!!!!!!!!
+            # Put your ModInfo variables here!
+            # Edit/Copy the ModInfo variables below as examples.
             ModInfo(i=0, name="Example Name", desc="This is the description!", active=False, enable_func=null_func, disable_func=null_func),
             ModInfo(i=1, name="Example Name 2", desc="This is the description! 2", active=False, enable_func=null_func), # disable_func isnt required
             ModInfo(i=2, name="No Title", desc="Sets the CMD window title to nothing.", active=False, enable_func=Mods.notitle)
