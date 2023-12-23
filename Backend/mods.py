2380b5d837d031556029a96c6bd0ddf2f79c6586
Fin-GITHUB <70870542+fin-github@users.noreply.github.com>
2023-12-23 16:35:15 -0500

    Create mods.py

---
+++ Backend/mods.py
@@ -0,0 +1,17 @@
+import requests as r
+from winsound import Beep
+from colorama import Fore, Style
+from os import system as cmd
+from time import sleep
+from Backend.classes import *
+
+
+class Modsclass:
+    def __init__(self):
+        # put all of your vars here
+        return
+    
+    def notitle(self):
+        cmd(f"TITLE ‎")
+        
+Mods = Modsclass()
